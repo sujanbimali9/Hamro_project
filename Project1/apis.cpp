14634e91894724d5793d7747a62002cac631c7ad
sujan bimali <sujanbimali999@gmail.com>
2024-03-13 20:28:03 +0545

    cart fixed

--- Project1/apis.cpp
+++ Project1/apis.cpp
@@ -81,9 +81,8 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, Response
     if (curl)
     {
         string url = "http://localhost:3000/get-order/"+id;
-        cout<< url << endl;
         Response response;
-        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
         // Set the write callback function
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
@@ -118,6 +117,42 @@ static size_t write_callback(void *contents, size_t size, size_t nmemb, Response
         return 1;
     }
 }
+
+ //int getCart(string id, vector<ProductCart>& product)
+ //{
+ //    CURL* curl;
+ //    curl = curl_easy_init();
+ //    Response response;
+ //    if (curl) {
+ //        curl_easy_setopt(curl,CURLOPT_URL, "http://localhost:3000/get-order");
+ //        string json = "{\"userid\": \"" + id + "\"}";
+
+ //        curl_easy_setopt(curl, CURLOPT_POSTFIELDS,json.c_str());
+ //        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, json.length());
+ //        struct curl_slist* headers = NULL;
+ //        headers = curl_slist_append(headers, "Content-Type: application/json");
+ //        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+ //        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+ //        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+ //        CURLcode res = curl_easy_perform(curl);
+
+ //        curl_slist_free_all(headers);
+ //        curl_easy_cleanup(curl);
+ //        if (res != CURLE_OK) {
+	//		 throw "error check your network conncetion and try again";
+	//		 return 1;
+	//	 }
+ //        else {
+	//		 parseProductsCart(response, product);
+	//		 return 0;
+	//	 }
+ //    }
+ //    else {
+ //        throw "curl not initialized properly";
+ //        return 0;
+ //    }
+ //}
 
  int orderFood(ProductCart &product)
 {
